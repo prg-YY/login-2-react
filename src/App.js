64a075dcfb2d407763dd76dd958227c58e99ac2e
prg0615 <parguzatt@gmail.com>
2022-01-10 18:41:01 +0100

    login-2 exercize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import "./App.css"
 
 function App() {
+  const initialValues = { username: "", email: "", password: "" }
+  const [formValues, setFormValues] = useState(initialValues)
+  const [errors, setErrors] = useState({})
+  const [isSubmit, setIsSubmit] = useState(false)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setErrors(validate(formValues))
+    setIsSubmit(true)
+  }
+
+  useEffect(() => {
+    console.log(errors)
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      console.log(formValues)
+    }
+  }, [errors])
+  const validate = (values) => {
+    const errors1 = {}
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if (!values.username) {
+      errors1.username = "UserName is required!"
+    }
+    if (!values.email) {
+      errors1.email = "Email is required!"
+    } else if (!regex.test(values.email)) {
+      errors1.email = "This is not valid email format"
+    }
+    if (!values.password) {
+      errors1.password = "Password is required!"
+    } else if (values.password.length < 4) {
+      errors1.password = "Password must be more than 4 characters"
+    } else if (values.password.length > 10) {
+      errors1.password = "Password cannot exceed more than 10 characters"
+    }
+    return errors1
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {Object.keys(errors).length === 0 && isSubmit ? (
+        <div className="ui message success">Signed in Successfully</div>
+      ) : (
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      )}
+      <form onSubmit={handleSubmit}>
+        <h1>Login Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>UserName</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="UserName"
+              value={formValues.username}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{errors.username}</p>
+
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formValues.email}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{errors.email}</p>
+
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formValues.password}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{errors.password}</p>
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
